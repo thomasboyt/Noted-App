9c3ec17c1c8ab4985a33b86abde5694da91860be
Thomas Boyt <me@thomasboyt.com>
2013-02-21 15:12:04 -0500

    added note export feature

--- app/assets/javascripts/models/note_model.js
+++ app/assets/javascripts/models/note_model.js
@@ -2,5 +2,28 @@ Noted.Note = DS.Model.extend({
   title: DS.attr('string'),
   created_date: DS.attr('date'),
 
-  listItems: DS.hasMany('Noted.ListItem')
+  listItems: DS.hasMany('Noted.ListItem'),
+
+  sortedItems: (function() {
+    return Ember.ArrayProxy.createWithMixins(Ember.SortableMixin, {
+      sortProperties: ['order'],
+      content: this.get('listItems')
+    })
+  }).property('listItems'),
+
+  serializeToTxt: function() {
+    var noteString = "";
+
+    noteString += this.get("title") + "\n\n";
+
+    this.get("sortedItems").forEach(function (item) {
+      // spacing based on the item's indentation
+      for (var i=0; i < item.get("indentionLevel"); i++) {
+        noteString += "    ";
+      }
+      noteString += "* " + item.get("text") + "\n";
+    });
+
+    return noteString;
+  }
 });
---
+++ app/assets/javascripts/views/bottom_controls_view.js
@@ -0,0 +1,11 @@
+Noted.BottomControlsView = Ember.View.extend({
+  exportNote: function() {
+    var txt = this.get("controller.selected").serializeToTxt();
+
+    $("#export-modal textarea").val(txt);
+    $("#export-modal").modal();
+    $("#export-modal").on("shown", function() {
+      $("#export-modal textarea").select();
+    })
+  }
+});
