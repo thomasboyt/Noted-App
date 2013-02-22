e79ae39c7db2bd6650de68abbdcb98d6e6e2424b
Thomas Boyt <me@thomasboyt.com>
2013-02-22 14:15:59 -0500

    added font-awesome to appcache

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Noted::Application.routes.draw do
 
-  # if Rails.env.production?
-  #   offline = Rack::Offline.configure :cache_interval => 120 do      
-  #     #cache ActionController::Base.helpers.asset_path("application.css")
-  #     #cache ActionController::Base.helpers.asset_path("application.js")
-  #     # cache other assets
-  #     network "/"  
-  #   end
-  #   match "/application.manifest" => offline  
-  # end
+  if Rails.env.production?
+    offline = Rack::Offline.configure :cache_interval => 120 do      
+      cache ActionController::Base.helpers.asset_path("application.css")
+      cache ActionController::Base.helpers.asset_path("application.js")
+      cache "assets/fontawesome-webfont.eot?v=3.0.2"
+      cache "assets/fontawesome-webfont.eot?#iefix&v=3.0.2"
+      cache "assets/fontawesome-webfont.woff?v=3.0.2"
+      cache "assets/fontawesome-webfont.ttf?v=3.0.2"
+      # cache other assets
+      network "/"  
+    end
+    match "/application.appcache" => offline  
+  end
 
-  match "/application.appcache" => Rails::Offline
+  # match "/application.appcache" => Rails::Offline
 
   root :to => 'assets#index'
 end
