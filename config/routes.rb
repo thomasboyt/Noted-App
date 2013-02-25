183f2e90ffec4688ef3d5af3b924c541b143ec0e
Thomas Boyt <me@thomasboyt.com>
2013-02-25 16:37:34 -0500

    allow connections to remote sites when cached

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Noted::Application.routes.draw do
 
   if Rails.env.production?
-    offline = Rack::Offline.configure :cache_interval => 120 do      
-      cache ActionController::Base.helpers.asset_path("application.css")
+    offline = Rack::Offline.configure :cache_interval => 120 do   
       cache ActionController::Base.helpers.asset_path("application.js")
+      cache ActionController::Base.helpers.asset_path("application.css")
+      
       cache "assets/fontawesome-webfont.eot?v=3.0.2"
       cache "assets/fontawesome-webfont.eot?#iefix&v=3.0.2"
       cache "assets/fontawesome-webfont.woff?v=3.0.2"
       cache "assets/fontawesome-webfont.ttf?v=3.0.2"
       # cache other assets
-      network "/"  
+      network "*"  
     end
     match "/application.appcache" => offline  
   end
